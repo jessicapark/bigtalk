f1f03884673931d37b3f1330330ce398be04e52c
JessicaPark <jessicapark@jessicaparkdeMacBook-Pro.local>
2014-11-16 19:05:40 +0800

    da

--- invitation2/main.js
+++ invitation2/main.js
@@ -65,6 +65,40 @@ function loader() {
 $(document).ready(function() {
   $('section').hide();
   $('.page1').show();
+          particlesJS('particles-js', {
+            particles: {
+                color: '#ff0314',
+                shape: 'circle',
+                opacity: 1,
+                size: 2.5,
+                size_random: true,
+                nb: 20,
+                line_linked: {
+                    enable_auto: true,
+                    distance: 250,
+                    color: '#ff0314',
+                    opacity: 0.5,
+                    width: 1,
+                    condensed_mode: {
+                        enable: false,
+                        rotateX: 600,
+                        rotateY: 600
+                    }
+                },
+                anim: {
+                    enable: true,
+                    speed: 2
+                }
+            },
+            interactivity: {
+                enable: true,
+                mouse: {
+                    distance: 200
+                },
+                mode: 'grab'
+            },
+            retina_detect: true
+        });
   var lll = setInterval("loader()", 2000);
 });
 window.onload = function() {
