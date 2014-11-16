fa7ce64025093a403931957fbb1af131b1f46daa
JessicaPark <jessicapark@jessicaparkdeMacBook-Pro.local>
2014-11-16 22:58:53 +0800

    dsad

--- invitation2/main.js
+++ invitation2/main.js
@@ -97,40 +97,40 @@ window.onload = function() {
   donghuabefore(1);
   donghua(1); //加载完成后自动播放第一页的动画
 
-  // particlesJS('particles-js', {
-  //     particles: {
-  //         color: '#ff0314',
-  //         shape: 'circle',
-  //         opacity: 1,
-  //         size: 2.5,
-  //         size_random: true,
-  //         nb: 20,
-  //         line_linked: {
-  //             enable_auto: true,
-  //             distance: 250,
-  //             color: '#ff0314',
-  //             opacity: 0.5,
-  //             width: 1,
-  //             condensed_mode: {
-  //                 enable: false,
-  //                 rotateX: 600,
-  //                 rotateY: 600
-  //             }
-  //         },
-  //         anim: {
-  //             enable: true,
-  //             speed: 2
-  //         }
-  //     },
-  //     interactivity: {
-  //         enable: true,
-  //         mouse: {
-  //             distance: 200
-  //         },
-  //         mode: 'grab'
-  //     },
-  //     retina_detect: true
-  // });
+  particlesJS('particles-js', {
+      particles: {
+          color: '#ff0314',
+          shape: 'circle',
+          opacity: 1,
+          size: 2.5,
+          size_random: true,
+          nb: 20,
+          line_linked: {
+              enable_auto: true,
+              distance: 250,
+              color: '#ff0314',
+              opacity: 0.5,
+              width: 1,
+              condensed_mode: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 600
+              }
+          },
+          anim: {
+              enable: true,
+              speed: 2
+          }
+      },
+      interactivity: {
+          enable: true,
+          mouse: {
+              distance: 200
+          },
+          mode: 'grab'
+      },
+      retina_detect: true
+  });
   
   onePageScroll(".main", {
     sectionContainer: "section",
